6e61b0652426bd513af984c9eaab9051164ee9b4
Adam Podsiadlo <adam.podsiadlo@holte.no>
2018-03-23 13:34:06 +0100

    Swap two integers

---
+++ CSharpEx/CSharpEx/Basics/Swapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEx.Basics
+{
+    public class Swapper
+    {
+        public Swapper(int a, int b)
+        {
+
+        }
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public void Swap()
+        {
+            
+        }
+    }
+}
