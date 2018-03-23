5e92e5ea69d4914c3c44b7caf1398323a7dd0045
Adam Podsiadło <13600045+AdamPodsiadlo@users.noreply.github.com>
2018-03-23 13:37:23 +0100

    Swap two integers (#1)

---
+++ CSharpEx/CSharpEx/Basics/Tests/SwapperTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSharpEx.Basics.Tests
+{
+    [TestFixture]
+    public class SwapperTests
+    {
+
+        [Test]
+        [TestCase(1, 2)]
+        [TestCase(2, 1)]
+        [TestCase(5, 5)]
+        [TestCase(9, 1234)]
+        [TestCase(0, 0)]
+        [TestCase(int.MinValue, int.MaxValue)]
+        public void Constructor(int a, int b)
+        {
+            var swapper = new Swapper(a,b);
+
+            Assert.AreEqual(a, swapper.A);
+            Assert.AreEqual(b, swapper.B);
+        }
+
+        [Test]
+        [TestCase(1,2)]
+        [TestCase(2,1)]
+        [TestCase(5,5)]
+        [TestCase(9,1234)]
+        [TestCase(0,0)]
+        [TestCase(int.MinValue, int.MaxValue)]
+        public void Swaps(int a, int b)
+        {
+            var swapper = new Swapper(a,b);
+
+            swapper.Swap();
+
+            Assert.AreEqual(b, swapper.A);
+            Assert.AreEqual(a, swapper.B);
+        }
+    }
+}
