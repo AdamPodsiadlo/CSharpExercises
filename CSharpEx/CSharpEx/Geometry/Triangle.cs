85741765471b80ec8f45fbbe654296098774f70b
AdamPodsialo <szturmovik@email.com>
2018-03-26 11:28:33 +0200

    Simple geometry - Triangle

---
+++ CSharpEx/CSharpEx/Geometry/Triangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEx.Geometry
+{
+    public class Triangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Triangle(double a, double b, double c)
+        {
+            
+        }
+
+        public bool IsEquilateral()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsIsosceles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsObtuse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAcute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double InCircleRadius()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double ExCircleRadius()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
